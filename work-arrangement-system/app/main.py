8a7dcbf3dba8987b3acf71a7e23aefc1cf6c9285
TiwKangXu <e0960467@u.nus.edu>
2026-05-29 13:50:10 +0800

    update APIs

--- work-arrangement-system/app/main.py
+++ work-arrangement-system/app/main.py
@@ -26,6 +26,22 @@ from app.work_arrangements.assignments.routes import (
     router as assignment_router
 )
 
+from app.work_arrangements.scheduling.models import (
+    ShiftDefinition
+)
+
+from app.work_arrangements.scheduling.assignment_models import (
+    ShiftAssignment
+)
+
+from app.work_arrangements.scheduling.routes import (
+    router as shift_definition_router
+)
+
+from app.work_arrangements.scheduling.assignment_routes import (
+    router as shift_assignment_router
+)
+
 app = FastAPI(
     title="Work Arrangement System"
 )
@@ -36,6 +52,8 @@ Base.metadata.create_all(bind=engine)
 app.include_router(employee_router)
 app.include_router(template_router)
 app.include_router(assignment_router)
+app.include_router(shift_definition_router)
+app.include_router(shift_assignment_router)
 
 @app.get("/")
 def root():
